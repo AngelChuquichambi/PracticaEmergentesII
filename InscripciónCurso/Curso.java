d080cfd792bc9ab54e5d7324a04cb59578824316
AngelChuquichambi <102180819+AngelChuquichambi@users.noreply.github.com>
2022-03-22 15:22:41 -0400

    Curso.java

---
+++ InscripciónCurso/Curso.java
@@ -0,0 +1,36 @@
+package com.emergentes;
+
+public class Curso {
+
+    private String nombre;
+    private String apellido;
+    private String curso;
+
+    public Curso() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+}
